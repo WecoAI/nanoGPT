9746cec17a8999a7c1f2d8a17a9b5f20bc964668
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2025-08-15 11:55:01 +0000

    update scripts to remove print statements for clearer terminal messages

--- optimized_model.py
+++ optimized_model.py
@@ -11,7 +11,7 @@ import inspect
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
-from baseline_inference import GPTModel
+from optimized_inference import GPTModel
 
 @dataclass
 class GPTConfig:
@@ -45,7 +45,7 @@ class GPT(GPTModel):
         # only dropout can be overridden see more notes below
         assert all(k == 'dropout' for k in override_args)
         from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
+        # print("loading weights from pretrained gpt: %s" % model_type)
 
         # n_layer, n_head and n_embd are determined from model_type
         config_args = {
@@ -54,13 +54,13 @@ class GPT(GPTModel):
             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
         }[model_type]
-        print("forcing vocab_size=50257, block_size=1024, bias=True")
+        # print("forcing vocab_size=50257, block_size=1024, bias=True")
         config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
         config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
         config_args['bias'] = True # always True for GPT model checkpoints
         # we can override the dropout rate, if desired
         if 'dropout' in override_args:
-            print(f"overriding dropout rate to {override_args['dropout']}")
+            # print(f"overriding dropout rate to {override_args['dropout']}")
             config_args['dropout'] = override_args['dropout']
         # create a from-scratch initialized minGPT model
         config = GPTConfig(**config_args)
